06756164ba61d760eef1f41fc8cb88de97c93660
Yatharth Pandey <yatharthpandey700@gmail.com>
2026-02-05 23:45:34 +0530

    added comment

--- string2.cpp
+++ string2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 #include<string>
 int main(){
-
+//bruh ts
     string name;
     cout<<"enter your name: ";
     getline(cin, name);
     cout<<"Your Name is :"<<name<<endl;    
-    
+
     return 0;
 }
