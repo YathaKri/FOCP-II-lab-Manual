2bd1b5f591064c5b3c020df9d0432a4ddd94c32b
Yatharth Pandey <yatharthpandey700@gmail.com>
2026-02-09 12:53:06 +0530

    started functions

---
+++ functions.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int add(int x,int y){
+    return x+y;
+}
+int sub(int x,int y){
+    return x-y;
+}
+int mult(int x,int y){
+    return x*y;
+}
+int divi(int x,int y){
+    return x/y;
+}
+int main(){
+    int choice;
+    int a,b;
+    int result;
+    cout<<"Enter 1st number:";
+    cin>>a;
+    cout<<"Enter 2nd number:";
+    cin>>b;
+    cout<<"Enter choice(1[add],2[sub],3[mult],4[div])";
+    cin>>choice;
+    if (choice==1) {
+        result=add(a,b);
+        cout<<result;
+    }
+    else if (choice==2) {
+        result=sub(a,b);
+        cout<<result;
+    }
+    else if (choice==3) {
+        result=mult(a,b);
+        cout<<result;
+    }
+    else if (choice==4) {
+        result=divi(a,b);
+        cout<<result;
+    }
+    
+
+    
+}
