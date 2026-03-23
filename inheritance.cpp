a864a005e9fbf385f31cb6ace4a120e3a450e1ab
Yatharth Pandey <yatharthpandey700@gmail.com>
2026-03-23 13:46:50 +0530

    class question

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,6 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    cout<<"lol"<<endl;
+
+
+class Vehicle {
+public:
+    string brand = "Ford";
+    int year = 2023;
+
+    void showParent() {
+        cout << "Parent function: I am a " << brand << " from " << year << endl;
+    }
+};
+
+
+class Car : public Vehicle {
+public:
+    string model = "Mustang";
+    int seatingCapacity = 4;
+
+    void showChild() {
+        cout << "Child function: This model is a " << model << " with " << seatingCapacity << " seats." << endl;
+    }
+};
+
+int main() {
+    Car myCar;
+
+    myCar.showParent();
+
+    myCar.showChild();
+
     return 0;
 }
